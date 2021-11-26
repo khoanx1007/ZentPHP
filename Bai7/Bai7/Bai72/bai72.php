cd77e6d1490926a92c01b9497005508c540d6bfa
khoanx1007 <90041062+khoanx1007@users.noreply.github.com>
2021-11-26 20:57:33 +0700

    Add files via upload

--- Bai7/Bai7/Bai72/bai72.php
+++ Bai7/Bai7/Bai72/bai72.php
@@ -2,20 +2,11 @@
 use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 function send_email($email_recive,$name,$contents,$subject){
-    //https://www.google.com/settings/security/lesssecureapps
-    // Khai báo thư viên phpmailer
             require 'src/Exception.php';
             require 'src/PHPMailer.php';
             require 'src/SMTP.php';
-    // Khai báo tạo PHPMailer
     $mail = new PHPMailer();
-    //Khai báo gửi mail bằng SMTP
     $mail->IsSMTP();
-    //Tắt mở kiểm tra lỗi trả về, chấp nhận các giá trị 0 1 2
-    // 0 = off không thông báo bất kì gì, tốt nhất nên dùng khi đã hoàn thành.
-    // 1 = Thông báo lỗi ở client
-    // 2 = Thông báo lỗi cả client và lỗi ở server
-    // To load the French version
     $mail->setLanguage('vi', '/optional/path/to/language/directory/');
     $mail->SMTPDebug  = 0;
             $mail->SMTPOptions = array (
@@ -25,29 +16,38 @@ function send_email($email_recive,$name,$contents,$subject){
             'allow_self_signed' => true)
             );
     $mail->CharSet="UTF-8";
-    $mail->Debugoutput = "html"; // Lỗi trả về hiển thị với cấu trúc HTML
-    $mail->Host       = "smtp.gmail.com"; //host smtp để gửi mail
-    $mail->Port       = 587; // cổng để gửi mail
-    $mail->SMTPSecure = "tls"; //Phương thức mã hóa thư - ssl hoặc tls
-    $mail->SMTPAuth   = true; //Xác thực SMTP
-    $mail->Username   = "hocphp.zent@gmail.com"; // Tên đăng nhập tài khoản Gmail
-    $mail->Password   = "****"; //Mật khẩu của gmail
-    $mail->SetFrom("zentgroup@gmail.com", "Zent Group"); // Thông tin người gửi
-    $mail->AddReplyTo("zentgroup@gmail.com","Zent Group");// Ấn định email sẽ nhận khi người dùng reply lại.
-    $mail->AddAddress($email_recive, $name);//Email của người nhận
-    //$mail->AddCC($email_recive, $name);//Email của người nhận
-    $mail->Subject = $subject; //Tiêu đề của thư
-    $mail->MsgHTML($contents); //Nội dung của bức thư.
-     //$mail->MsgHTML(file_get_contents("email-template.html"), dirname(__FILE__));
-    // Gửi thư với tập tin html
-    $mail->AltBody = "Nội dung thư";//Nội dung rút gọn hiển thị bên ngoài thư mục thư.
-    //$mail->AddAttachment("images/attact-tui.gif");//Tập tin cần attach
-
-    //Tiến hành gửi email và kiểm tra lỗi
+    $mail->Debugoutput = "html"; 
+    $mail->Host       = "smtp.gmail.com"; 
+    $mail->Port       = 587; 
+    $mail->SMTPSecure = "tls"; 
+    $mail->SMTPAuth   = true; 
+    $mail->Username   = "khoanx1007@gmail.com";
+    $mail->Password   = "lamkhoa1997";
+    $mail->SetFrom("lambeo107@gmail.com", "Zent Group"); 
+    $mail->AddReplyTo("lambeo107@gmail.com","Zent Group");
+    $mail->AddAddress($email_recive, $name);
+    $mail->Subject = $subject; 
+    $mail->MsgHTML($contents); 
+    $mail->AltBody = "Nội dung thư";
     if(!$mail->Send()) {
                 return false;
     } else {
                 return true;
     }
 }
+$contents = '
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>';
+$subject = 'Test send email';
+send_email("lambeo107@gmail.com", "Nguyễn Xuân Khoa",$contents,$subject)
 ?>
